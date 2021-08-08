e49433352109c699f1f75d1950dd689f586334b1
dParikesit <dimasparikesitt@gmail.com>
2021-08-08 09:33:42 +0700

    feat: refactored

--- models/sales.go
+++ models/sales.go
@@ -1,21 +1,7 @@
 package models
 
-type sales struct {
-        data map[int]int
-}
-
 // TODO: instantiate from transaction model
 func NewSales(d []int) Transactions {
-        data := make(map[int]int)
-
-        for i, v := range d {
-                data[i] = v
-        }
-
-        // i.e. return NewTransactions(d)
-        return &sales{data: data}
+        return NewTransactions(d)
 }
 
-func (s *sales) Get(idx int) int {
-        return s.data[idx]
-}
